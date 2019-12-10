7d02ec0f64dc78d6b8b6e66c0856102e4bd82b24
Andy Park <atpark333@gmail.com>
2019-12-10 12:55:39 +0900

    Update entry point

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const convertHtmlToPdf = require('pdf-puppeteer');
 const api = require('./lib/api');
+const formatter = require('./lib/formatter');
 
-module.exports = {
-  run(username) {
-    api.getProfile(username, this.format);
-  },
-  format(profileData) {
-    console.log(profileData);
-  }
+module.exports = function(username) {
+  api.getProfile(username, formatter.toHtml);
 }
+
+require('./index')('atparkweb')
