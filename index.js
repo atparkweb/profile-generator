541a42dcc3d91c5eceb71a09d05861ee0f84b3d8
Andy Park <atpark333@gmail.com>
2019-12-10 13:03:22 +0900

    Refactor api and formatter

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const api = require('./lib/api');
 const formatter = require('./lib/formatter');
 
 module.exports = function(username) {
-  api.getProfile(username, formatter.toHtml);
+  api.getProfile(username).then(function(data) {
+    console.log(data)
+    convertHtmlToPdf(formatter.toHtml(data), function(pdf) {
+      // TODO: Save the pdf content somewhere
+    });
+  });
 }
 
 require('./index')('atparkweb')
