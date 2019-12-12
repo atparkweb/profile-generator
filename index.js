6206fd017bb0c0468267db144c08e3e1bab5c779
Andy Park <atpark333@gmail.com>
2019-12-12 09:20:40 +0900

    Add comment

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const formatter = require('./lib/formatter');
 const fs = require('fs');
 
 module.exports = function () {
+  // Get username from command line argument
   let username = process.argv[2]
   if (!username) {
     console.warn('No username provided. Usage: npm start [username]')
     return;
   }
+
   api.getProfile(username).then(function(data) {
     convertHtmlToPdf(formatter.toHtml(data), function(pdf) {
       fs.writeFileSync(`./output/${username}.pdf`, pdf, 'binary');
